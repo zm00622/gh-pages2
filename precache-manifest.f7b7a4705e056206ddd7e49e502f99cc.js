4bae49994a9ce832ebf6c0230aba697c014e14b5
zm00622 <zarymanning@gmail.com>
2020-02-01 15:33:30 -0500

    Updates

---
+++ precache-manifest.f7b7a4705e056206ddd7e49e502f99cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d2a12fdee71d8f88859ca6eb418e37",
+    "url": "/gh-pages2/index.html"
+  },
+  {
+    "revision": "fe050a261fb71c47e265",
+    "url": "/gh-pages2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9aa150bfc28531889428",
+    "url": "/gh-pages2/static/js/2.dd88c35f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages2/static/js/2.dd88c35f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe050a261fb71c47e265",
+    "url": "/gh-pages2/static/js/main.ba985832.chunk.js"
+  },
+  {
+    "revision": "f385117c6707ac7c0988",
+    "url": "/gh-pages2/static/js/runtime-main.c36c76cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages2/static/media/logo.5d5d9eef.svg"
+  }
+]);
